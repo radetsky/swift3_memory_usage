64d4309d0b2f2892469d86c2416ee997fb833235
Alex Radetsky <r.alexey@dizboard.com>
2017-02-24 13:11:26 +0200

    Initial Commit

---
+++ freeMemoryTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  freeMemoryTest
+//
+//  Created by Alex Radetsky on 2/24/17.
+//  Copyright © 2017 User Mac. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
